57e80d211fd6ce3cfa29ba58496dd3fea36fdcde
Syed Rahim <84041518+Chamkani17@users.noreply.github.com>
2021-05-12 07:49:19 +0500

    Update Done.py

--- Done.py
+++ Done.py
@@ -153,8 +153,8 @@ logo2 =
 \033[1; 92m➾ NOTE : USE 4G FAST INTERNET
 \033[1;95m➾ NOTE : NO NEED PROXY/VPN 
 \033[1;97m∙∙·▫▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ PUHTOON BRAND ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ▫▫·∙∙                 
-"""                         
-       """                   
+                       
+                       
 ##### LICENSE #####
 #=================#
 def lisensi():
